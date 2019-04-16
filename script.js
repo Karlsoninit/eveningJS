677bdda5e40e071d97e0fa67e5c26eec101bf050
Karlsoninit <m.norets@gmail.com>
2019-04-16 18:18:40 +0300

    added module07

---
+++ script.js
@@ -0,0 +1,97 @@
+'use strict'
+
+
+
+
+// let showBox = document.querySelector('.list')
+// console.log(showBox.children[0]);
+
+
+// let find = document.querySelector('');
+
+// find.onclick = function(){
+//     find.style.color = 'red';
+//     find.style.fontSize = '90px';
+//     find.textContent = 'its true!!'
+// }
+
+// console.log(find);
+
+// let text = document.querySelector('.text');
+// console.log(text)
+
+// let input = document.querySelector('.input');
+// console.log(input);
+
+// let button = document.querySelector('.click');
+
+// console.log(button);
+
+// function test() {
+//     if( input.value.length > 0){
+//         text.className = 'textColor';
+//         text.textContent = input.value;
+//     }
+// }
+
+
+
+
+// button.addEventListener('click', test);
+
+
+// let mass = ['html', 'css', 'react', 'c++','JavaScript'];
+
+// let showList = document.querySelector('.list');
+// console.log(showList);
+
+
+// let result = mass.reduce((string, el) => string  + `<li>${el}</li>`, "");
+// showList.style.fontSize = '50px';
+// showList.innerHTML += result;
+
+// console.log('show this',showList.children[2].style.color = 'red');
+
+
+
+
+
+//----------------------------------
+
+
+
+
+
+
+
+// let btnLeft = document.querySelector('.left');
+// console.log(btnLeft)
+// let btnRight = document.querySelector('.right');
+// console.log(btnRight)
+
+// let images = document.querySelectorAll('img');
+// console.log(images);
+
+// let i = 0;
+
+// btnRight.onclick = function(){
+//     images[i].className = ''
+//     i++
+//     if(i > images.length -1){
+//         i = 0
+//     }
+
+//     setTimeout(function(){
+
+//         images[i].className = 'showed';
+//     },2000)
+// }
+
+// btnLeft.onclick = function(){
+//     images[i].style.display = 'none';
+//     i--
+//     if(i < 0){
+//         i = images.length -1
+//     }
+//     images[i].style.display = 'block';
+// }
